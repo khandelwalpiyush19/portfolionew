df7422be37e2be1c88ae9e1f6a493b7c5b3d040c
khandelwalpiyush19 <piyushkhandelwal482@gmail.com>
2025-03-17 17:34:41 +0530

    commit

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -42,28 +42,29 @@ const Contact = () => {
         {/* Radial Gradient to prevent sharp edges */}
         <div className="absolute inset-0 w-full h-full bg-black [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
         <h1 className='text-white'>Additional links </h1>
-        <div className='grid grid-cols-3 md:grid-cols-5 gap-10 justify-center items-center mt-5'>
-          <Link href="https://github.com/piyushkhandelwal482" className="inline-flex items-center text-blue-500 hover:text-blue-600">
-            <IconBrandGithub className="w-5 h-5 mr-2" />
-            Github
-          </Link>
-          <Link href="https://www.linkedin.com/in/piyush-khandelwal-b9824019b/" className="inline-flex items-center text-blue-500 hover:text-blue-600">
-            <IconBrandLinkedin className="w-5 h-5 mr-2" />
-            LinkedIn
-          </Link>
-          <Link href="https://github.com/piyushkhandelwal482" className="inline-flex items-center text-blue-500 hover:text-blue-600">
-            <IconHome className="w-5 h-5 mr-2" />
-            Github
-          </Link>
-          <Link href="https://www.linkedin.com/in/piyush-khandelwal-b9824019b/" className="inline-flex items-center text-blue-500 hover:text-blue-600">
-            <IconBrandYoutube className="w-5 h-5 mr-2" />
-            LinkedIn
-          </Link>
-          <Link href="https://www.linkedin.com/in/piyush-khandelwal-b9824019b/" className="inline-flex items-center text-blue-500 hover:text-blue-600">
-            <IconBrandTwitter className="w-5 h-5 mr-2" />
-            LinkedIn
-          </Link>
-        </div>
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6 justify-center items-center mt-5">
+  <Link href="https://github.com/piyushkhandelwal482" className="inline-flex items-center text-blue-500 hover:text-blue-600">
+    <IconBrandGithub className="w-6 h-6 sm:w-8 sm:h-8 mr-2" />
+    <span className="hidden md:inline">Github</span>
+  </Link>
+  <Link href="https://www.linkedin.com/in/piyush-khandelwal-b9824019b/" className="inline-flex items-center text-blue-500 hover:text-blue-600">
+    <IconBrandLinkedin className="w-6 h-6 sm:w-8 sm:h-8 mr-2" />
+    <span className="hidden md:inline">LinkedIn</span>
+  </Link>
+  <Link href="https://github.com/piyushkhandelwal482" className="inline-flex items-center text-blue-500 hover:text-blue-600">
+    <IconHome className="w-6 h-6 sm:w-8 sm:h-8 mr-2" />
+    <span className="hidden md:inline">Portfolio</span>
+  </Link>
+  <Link href="https://www.linkedin.com/in/piyush-khandelwal-b9824019b/" className="inline-flex items-center text-blue-500 hover:text-blue-600">
+    <IconBrandYoutube className="w-6 h-6 sm:w-8 sm:h-8 mr-2" />
+    <span className="hidden md:inline">YouTube</span>
+  </Link>
+  <Link href="https://www.linkedin.com/in/piyush-khandelwal-b9824019b/" className="inline-flex items-center text-blue-500 hover:text-blue-600">
+    <IconBrandTwitter className="w-6 h-6 sm:w-8 sm:h-8 mr-2" />
+    <span className="hidden md:inline">Twitter</span>
+  </Link>
+</div>
+
       </div>
     </div>
   
